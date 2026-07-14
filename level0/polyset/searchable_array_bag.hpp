96cc92a5c903bc334a850eb8b8db0c92eae1cd7b
Said Echlahbi <sechlahb@c1r8p1.1337.ma>
2026-07-14 19:49:09 +0100

    update

---
+++ level0/polyset/searchable_array_bag.hpp
@@ -0,0 +1,16 @@
+#ifndef SEARCHABLE_ARRAY_BAG_HPP
+#define SEARCHABLE_ARRAY_BAG_HPP
+
+
+#include "array_bag.hpp"
+
+class searchable_array_bag: public array_bag
+{
+    public:
+
+};
+
+
+
+
+#endif /*SEARCHABLE_ARAY_BAG_HPP*/
---
+++ level0/polyset/searchable_tree_bag.hpp
@@ -0,0 +1,18 @@
+#ifndef SEARCHABLE_TREE_BAG
+#define SEARCHABLE_TREE_BAG
+
+#include "tree_bag.hpp"
+
+class searchable_tree_bag: public tree_bag
+{
+    private:
+        /* data */
+    public:
+        searchable_tree_bag(/* args */);
+        ~searchable_tree_bag();
+};
+
+
+
+
+#endif /*SEARCHABLE_TREE_BAG*/
