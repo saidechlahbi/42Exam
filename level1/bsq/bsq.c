547aaa7321aaa681374aa276aaa2abe07b8a6ff2
Said Echlahbi <sechlahb@c1r7p7.1337.ma>
2026-07-22 09:23:19 +0100

    good game

--- level1/bsq/bsq.c
+++ level1/bsq/bsq.c
@@ -1,6 +1,88 @@
-#include <stdio.h>
+#include "stdio.h"
+#include "stdlib.h"
 
-int main ()
+int slen(char *s)
 {
-    
+	int i = 0;
+	for ( ;s[i]; i++);
+	return i;
+}
+int min(int a, int b, int c)
+{
+	return a < b ? (a < c ? a : c) : (b < c ? b : c);
+}
+int bsq(FILE *file)
+{
+	char empty = 0, obstacle = 0, full = 0;
+	int height = 0;
+	if (fscanf(file, "%d%c%c%c\n", &height, &empty, &obstacle, &full) != 4)
+		return 1;
+	if (height <= 0 || empty == obstacle || empty == full || obstacle == full)
+		return 1;
+	char** map = calloc(height, sizeof(char*));
+	int i = 0, w;
+	char *line = NULL;
+	size_t len = 0;
+	for (; getline(&line, &len, file) != -1; i++)
+	{
+		if (i == height)
+			return 1;
+		map[i] = line;
+		line = NULL;
+		len = 0;
+		if (i == 0)
+		{
+			w = slen(map[0]);
+			if (w < 2 || map[0][w-1] != '\n')
+				return 1;
+		}
+		else if (w != slen(map[i]) || map[i][w-1] != '\n')
+			return 1;
+	}
+	if (i < height)
+		return 1;
+	////////////////////////////////////////////////////////////
+	int **dp = calloc(height + 1, sizeof(int*));
+	int y = 0, x = 0, size = 0;
+	for (int i = 0; i <= height; i++)
+		dp[i] = calloc(w + 1, sizeof(int));
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < w - 1; j++)
+		{
+			if (map[i][j] == empty)
+			{
+				dp[i+1][j+1] = 1 + min(dp[i][j], dp[i+1][j], dp[i][j+1]);
+				if (size < dp[i+1][j+1])
+					y = i, x = j, size = dp[i+1][j+1]; 
+			}
+			else if (map[i][j] != obstacle)
+				return 1;
+		}
+	}
+	for (int i = y - size + 1; i <= y; i++)
+	{
+		for (int j = x - size + 1; j <= x; j++)
+			map[i][j] = full;
+	}
+	for (int i = 0; i < height; i++)
+		fprintf(stdout, "%s", map[i]);
+	return 0;
+}
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		bsq(stdin);
+	else if (ac == 2)
+	{
+		FILE *file = fopen(av[1], "r");
+		if (!file || bsq(file))
+			fprintf(stderr, "Error: invalid map\n");
+		if (file)
+			fclose(file);
+	}
+	else
+		fprintf(stderr, "Error: invalid map\n");
+
+	return 0;
 }
